f1dab9c4fe3543abb65d7ac6facae16e214ce385
cichan <6684177pt@gmail.com>
2022-09-25 16:50:29 +0300

    small update

--- HelloFx/src/application/Main.java
+++ HelloFx/src/application/Main.java
@@ -17,8 +17,8 @@ public class Main extends Application {
 	public void start(Stage stage) throws Exception {
 		Group root = new Group(); 
 		Scene scene = new Scene(root, Color.BLUE);
-		Image icon = new Image("https://www.shareicon.net/data/2015/09/20/643803_technology_512x512.png");
 		
+		Image icon = new Image("https://www.shareicon.net/data/2015/09/20/643803_technology_512x512.png");		
 		stage.getIcons().add(icon);
 		stage.setTitle("Stage demo-program");
 		stage.setWidth(420);
@@ -26,11 +26,11 @@ public class Main extends Application {
 		//stage.setResizable(false);
 		//stage.setX(50);
 		//stage.setY(50);
-		//stage.setRenderScaleX(50);
-		//stage.setRenderScaleY(50);
-		stage.setFullScreen(true);
-		stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
-		stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+		stage.setRenderScaleX(50);
+		stage.setRenderScaleY(50);
+		//stage.setFullScreen(true);
+		//stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
+		//stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
 		
 		stage.setScene(scene);
 		stage.show();
