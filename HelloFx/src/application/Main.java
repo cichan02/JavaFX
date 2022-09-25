1877b33acb81e45b8419ae640b730590ac8c00b3
cichan <6684177pt@gmail.com>
2022-09-25 16:43:27 +0300

    undestanding stages in javafx

--- HelloFx/src/application/Main.java
+++ HelloFx/src/application/Main.java
@@ -1,25 +1,38 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Group root = new Group(); 
+		Scene scene = new Scene(root, Color.BLUE);
+		Image icon = new Image("https://www.shareicon.net/data/2015/09/20/643803_technology_512x512.png");
+		
+		stage.getIcons().add(icon);
+		stage.setTitle("Stage demo-program");
+		stage.setWidth(420);
+		stage.setHeight(420);
+		//stage.setResizable(false);
+		//stage.setX(50);
+		//stage.setY(50);
+		//stage.setRenderScaleX(50);
+		//stage.setRenderScaleY(50);
+		stage.setFullScreen(true);
+		stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
+		stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+		
+		stage.setScene(scene);
+		stage.show();
 	}
 }
