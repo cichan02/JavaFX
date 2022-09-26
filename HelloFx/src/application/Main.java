d6c059be6c01537ab02dc6d86860c8bfda4f4749
cichan <6684177pt@gmail.com>
2022-09-26 12:28:12 +0300

    undestanding css

--- HelloFx/src/application/Main.java
+++ HelloFx/src/application/Main.java
@@ -1,11 +1,10 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 
 public class Main extends Application {
 	public static void main(String[] args) {
@@ -14,22 +13,10 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Group root = new Group(); 
-		Scene scene = new Scene(root, Color.BLUE);
-		
-		Image icon = new Image("https://www.shareicon.net/data/2015/09/20/643803_technology_512x512.png");		
-		stage.getIcons().add(icon);
-		stage.setTitle("Stage demo-program");
-		stage.setWidth(420);
-		stage.setHeight(420);
-		//stage.setResizable(false);
-		//stage.setX(50);
-		//stage.setY(50);
-		stage.setRenderScaleX(50);
-		stage.setRenderScaleY(50);
-		//stage.setFullScreen(true);
-		//stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
-		//stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(root);
+		String css = this.getClass().getResource("application.css").toExternalForm();
+		scene.getStylesheets().add(css);
 		
 		stage.setScene(scene);
 		stage.show();
