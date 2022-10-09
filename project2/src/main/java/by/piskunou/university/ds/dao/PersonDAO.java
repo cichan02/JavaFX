25a9b1f5b941488955c2542626bbc21665f552f8
cichan <6684177pt@gmail.com>
2022-10-09 15:47:04 +0300

    add layers

---
+++ project2/src/main/java/by/piskunou/university/ds/dao/PersonDAO.java
@@ -0,0 +1,10 @@
+package by.piskunou.university.ds.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import by.piskunou.university.ds.models.Person;
+
+public class PersonDAO {
+	private List<Person> people = new ArrayList<>(4);
+}
