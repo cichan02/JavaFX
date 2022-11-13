e2d3d63acc23300fa49c2afb77ff46345df85a70
cichan <6684177pt@gmail.com>
2022-11-13 22:44:32 +0300

    project3 init

---
+++ project3/src/main/java/by/piskunou/university/ds/MainApp.java
@@ -0,0 +1,23 @@
+package by.piskunou.university.ds;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+    	Parent root = loader.load();
+    	Scene scene = new Scene(root);
+
+        stage.setTitle("Project 2: Demo");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
